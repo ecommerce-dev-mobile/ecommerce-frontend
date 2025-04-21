5b4908f21aa0a20f4ce3b145449eb02f18e06435
joaovnpt <joaovitoguiotti8@gmail.com>
2025-04-21 14:58:54 -0300

    Style: Criar layout dos cards de produtos

--- src/views/ProductsListView.vue
+++ src/views/ProductsListView.vue
@@ -6,7 +6,17 @@ const carStore = useCarStore()
 </script>
 
 <template>
-  <ProductComponent v-for="car in carStore.cars" :key="car.id" :product="car" />
+  <div class="products-container">
+    <ProductComponent v-for="car in carStore.cars" :key="car.id" :product="car" />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.products-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 2rem;
+}
+</style>

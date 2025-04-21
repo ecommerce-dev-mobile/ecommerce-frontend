1bd53be61e3800f63925cd5fe283feaccaa1bf89
joaovnpt <joaovitoguiotti8@gmail.com>
2025-04-21 13:13:42 -0300

    Feat: Criar página para listagem de produtos

---
+++ src/views/ProductsListView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import ProductComponent from '@/components/ProductComponent.vue'
+import { useCarStore } from '@/stores/car'
+
+const carStore = useCarStore()
+</script>
+
+<template>
+  <ProductComponent v-for="car in carStore.cars" :key="car.id" :product="car" />
+</template>
+
+<style scoped></style>

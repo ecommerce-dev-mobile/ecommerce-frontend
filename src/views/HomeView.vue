bf47f139b2629851bd76fd40df76ebc0e7e88521
joaovnpt <joaovitoguiotti8@gmail.com>
2025-04-21 14:59:17 -0300

    Feat: Adicionar ProductListView na HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup>
 import IntroSection from '@/components/IntroSection.vue'
+import ProductsListView from './ProductsListView.vue';
 </script>
 
 <template>
   <main>
     <IntroSection />
+    <ProductsListView />
   </main>
 </template>
 

9de7cecda56ee0cec4bea7810f8f010bcede6a50
joaovnpt <joaovitoguiotti8@gmail.com>
2025-04-21 13:13:09 -0300

    Feat: Criar Product card container

---
+++ src/components/ProductComponent.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+    product: Object
+})
+</script>
+
+<template>
+    <div class="product-card">
+        <img class="product-image" :src="product.imageURL" alt="">
+        <p class="product-title">{{ product.name }}</p>
+        <p class="product-category">{{ product.category }}</p>
+        <p class="product-price">{{ product.price.toFixed(2).replace('.',',') }}</p>
+    </div>
+</template>
+
+<style scoped>
+.product-card {
+    display: flex;
+    flex-direction: column;
+}
+</style>

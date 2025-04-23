ac141bb2eb82685c15fb0b26244a7742bc6a2675
joaovnpt <joaovitoguiotti8@gmail.com>
2025-04-23 15:03:07 -0300

    FEAT: Criar categorias para carros

---
+++ src/constants/categories.js
@@ -0,0 +1,11 @@
+export const categories = [
+    {
+        id: 'racing-cars', label: 'Racing Cars',
+    },
+    {
+        id: 'classics', label: 'Classics',
+    },
+    {
+        id: 'support-cars', label: 'Support Cars',
+    },
+]

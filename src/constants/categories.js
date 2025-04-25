03481a9806c75ee685d6e1179dcecb39eb53f7d6
joaovnpt <joaovitoguiotti8@gmail.com>
2025-04-25 14:28:06 -0300

    FEAT: Mudar nome das categorias

--- src/constants/categories.js
+++ src/constants/categories.js
@@ -1,11 +1,11 @@
 export const categories = [
     {
-        id: 'racing-cars', label: 'Racing Cars',
+        id: 'racing', label: 'Racing Cars',
     },
     {
         id: 'classics', label: 'Classics',
     },
     {
-        id: 'support-cars', label: 'Support Cars',
+        id: 'support', label: 'Support Cars',
     },
 ]

99c1918b34c837b2ab55859b934f6011b550d667
joaovnpt <joaovitoguiotti8@gmail.com>
2025-04-26 23:55:35 -0300

    FEAT: Remover footer da homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,13 @@
 <script setup>
 import IntroSection from '@/components/IntroSection.vue'
 import ProductsListView from './ProductsListView.vue';
-import Footer from '@/components/FooterComponent.vue'
+
 </script>
 
 <template>
   <main>
     <IntroSection />
     <ProductsListView />
-    <Footer></Footer>
   </main>
 </template>
 
